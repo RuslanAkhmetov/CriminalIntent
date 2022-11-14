7053ae650f883d0b7ebbdb1fc0d370f4eb15e686
RuslanAkhmetov <akhmrusl@gmail.com>
2022-11-14 17:22:31 +0300

    Chapter 9 Home task1

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -2,8 +2,9 @@ import java.util.Date
 import java.util.UUID
 
 data class Crime(
-    val id: UUID = java.util.UUID.randomUUID(),
+    val id: UUID = UUID.randomUUID(),
     var title: String = "",
     var date: Date = java.util.Date(),
-    var isSolved: Boolean = false
+    var isSolved: Boolean = false,
+    var requiresPolice: Boolean = false
 )
